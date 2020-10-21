0f84cbd669585c19502f9c9e600123ed266b0367
sheny <fsbydz@vip.qq.com>
2020-10-22 00:48:23 +0800

    调试打包，就差oracle和mssql包就可以登录了

---
+++ wechat/src/main/java/cn/besbing/wechat/WechatApplication.java
@@ -0,0 +1,23 @@
+package cn.besbing.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright(C),2019-2020,厦门星港网络科技有限公司
+ * FileName:WechatApplication
+ * Author:  sheny
+ * Date:    2020/10/22 : 0:11
+ * Description:
+ * History:
+ * <author>    <time>     <version>      <desc>
+ * 作者姓名    修改时间       版本号         描述
+ */
+@SpringBootApplication
+public class WechatApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(WechatApplication.class, args);
+        }
+
+}
